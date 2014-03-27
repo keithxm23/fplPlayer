0f30d84fb66207f4bbcfac79e18ccee80570ea61
Keith Mascarenhas <keithxm23@gmail.com>
2014-03-26 23:04:08 -0400

    made Games also factory model

--- games.py
+++ games.py
@@ -9,22 +9,42 @@ class Games:
     # load them all to same self.games attribute
     # TODO Use builder factory pattern so as to make methods chainable
     #  that is the return type should also be of type Games()
-    def __init__(self, csvfile):
-        with open(csvfile, 'r') as f:
-            reader = csv.reader(f)
-            header = reader.next()
-            self.games = [Game(row, header) for row in reader]
+    def __init__(self, feed):
+        if type(feed) is str:
+            with open(feed, 'r') as f:
+                reader = csv.reader(f)
+                header = reader.next()
+                self.games = [Game(row, header) for row in reader]
+        elif type(feed) is list:
+            self.games = feed
 
     def of_team(self, teamname):
-        return [g for g in self.games if Team(teamname) in [g.hometeam, g.awayteam]]
+        return Games([g for g in self.games if Team(teamname) in [g.hometeam,
+            g.awayteam]])
 
 
     def of_team_at_home(self, teamname):
-        return [g for g in self.games if teamname == g.hometeam]
+        return Games([g for g in self.games if teamname == g.hometeam])
    
 
     def of_team_at_away(self, teamname):
-        return [g for g in self.games if teamname == g.awayteam]
+        return Games([g for g in self.games if teamname == g.awayteam])
+
+    def points_earned_for(self, teamname):
+        points_earned = 0
+        for g in self.games:
+            if teamname == g.hometeam:
+                points_earned+=g.homepoints
+            elif teamname == g.awayteam:
+                points_earned+=g.awaypoints
+        return points_earned
+
+    def points_progression_for(self, teamname):
+        points = [0,] # seed value for points for a team
+        for index, g in enumerate(self.games):
+            points.append(points[index]+g.points_for(teamname))
+        points.pop(0) # we do not need the seed value for points
+        return points
 
 
 
